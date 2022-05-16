ddf58c0f3392d69330ef58d0e53b73c16903cef1
Juan Jesus Lozano <garbagetin1@gmail.com>
2022-05-16 12:58:44 +0200

    error uno

---
+++ src/paqueteUno/ErrorUNO.java
@@ -0,0 +1,11 @@
+package paqueteUno;
+
+public class ErrorUNO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("cada vez tengo menos tiempo, y mas cosas que hacer")
+
+	}
+
+}
